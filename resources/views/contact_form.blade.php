6bcafc9572ef7111b2fe3f06fa3e20b89d867d4a
QuintanillaAdrián <adrianq1299@gmail.com>
2025-03-11 15:15:47 -0600

    .

--- resources/views/contact_form.blade.php
+++ resources/views/contact_form.blade.php
@@ -10,6 +10,7 @@
 <body>
     <h1>Formulario de contacto</h1>
     <form action="/submit-contact" method="POST">
+    @csrf
         <label for="name">Nombre:</label>
         <input type="text" id="name" name="name" required><br><br>
 
